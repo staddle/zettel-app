8515b638d13c40442e83178383237251dbb4243b
Niclas Rosteck <hirofushikar@gmail.com>
2022-12-14 14:20:34 +0100

    Version Increment

--- src/assets/globalSettings.js
+++ src/assets/globalSettings.js
@@ -1,6 +1,6 @@
 export default {
   // The version number of the app
-  version: '2022.12.3',
+  version: '2022.12.4',
   // The default language for the app
   defaultLanguage: 'en',
   // The default currency for the app
